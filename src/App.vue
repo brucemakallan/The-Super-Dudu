84cfbb80510366656ba1d64904b325287b8e7cf5
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-19 13:54:29 +0300

    Custom Loading component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
     <notifications group="notification-group" width="100%" />
+    <Loading />
     <transition name="fade" mode="out-in">
       <router-view/>
     </transition>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Loading from './components/Loading.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Loading,
+  },
+});
+</script>
+
 
 <style lang="scss">
 @import "./assets/styles/global.scss";
